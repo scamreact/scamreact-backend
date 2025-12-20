23092889d7aed7238c8c54129ae25c704c0f0283
gioarca <giorgio.arcamone93@gmail.com>
2025-12-20 15:53:09 +0100

    borgo changed

---
+++ Untitled-1.js
@@ -0,0 +1,48 @@
+// {
+//   "name": "api-borghi",
+//   "version": "1.0.0",
+//   "description": "the last dance",
+//   "main": "index.js",
+//   "scripts": {
+//     "start": "nodemon src/index.js",
+//     "build": "nodemon build src/index.js",
+//     "dev": "nodemon src/index.js"
+//   },
+//   "author": "",
+//   "license": "ISC",
+//   "dependencies": {
+//     "@anthropic-ai/sdk": "^0.65.0",
+//     "axios": "^1.12.2",
+//     "bcryptjs": "^2.4.3",
+//     "body-parser": "^1.20.3",
+//     "cloudinary": "^2.5.1",
+//     "config": "^3.3.12",
+//     "cookie-parser": "^1.4.7",
+//     "cors": "^2.8.5",
+//     "cross-env": "^7.0.3",
+//     "date-fns": "^2.0.0",
+//     "date-fns-tz": "^2.0.0",
+//     "dotenv": "^16.4.7",
+//     "express": "^4.21.2",
+//     "express-mongo-sanitize": "^2.2.0",
+//     "express-validator": "^7.2.1",
+//     "firebase": "^11.1.0",
+//     "helmet": "^8.0.0",
+//     "i18n": "^0.15.1",
+//     "jsonwebtoken": "^9.0.2",
+//     "mongodb": "^6.15.0",
+//     "mongodb-memory-server": "^10.1.3",
+//     "mongoose": "^8.9.4",
+//     "mongoose-paginate-v2": "^1.9.0",
+//     "node": "^22.13.0",
+//     "node-cron": "^4.2.1",
+//     "nodemailer": "^7.0.9",
+//     "nodemon": "^3.1.9",
+//     "speakeasy": "^2.0.0",
+//     "stripe": "^19.1.0",
+//     "xss-clean": "^0.1.4"
+//   },
+//   "devDependencies": {
+//     "@types/bcryptjs": "^2.4.6"
+//   }
+// }
